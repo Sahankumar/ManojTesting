024b32edd14561bce50bca35171c256611adc0cd
kishore <kishoresanapala@gmail.com>
2016-01-06 22:40:58 +0530

    Ant-Maven

---
+++ Copy of Exploring/src/com/test/ant/Dummy.java
@@ -0,0 +1,13 @@
+package com.test.ant;
+
+import org.testng.annotations.Test;
+
+public class Dummy {
+	
+	@Test
+	public void addNumbers(){
+		System.out.println("addNumbers");
+		System.out.println("Hello");
+	}
+
+}
